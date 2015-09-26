bf26cb97ca2e23b15fe0675bc8b5e2b751dcdec0
badlydrawnmuiry <badlydrawnmuiry@hotmail.com>
2015-09-26 15:11:37 +0100

    Adopter Project
    Initial setup

---
+++ survey/surveyPortal/src/main/java/com/adopter/SurveyPortal/App.java
@@ -0,0 +1,13 @@
+package com.adopter.SurveyPortal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
